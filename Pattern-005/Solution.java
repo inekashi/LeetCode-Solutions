0014ebd78026c2ca95aa8619bd74287118c40893
PIYUSH JHA <154626548+inekashi@users.noreply.github.com>
2026-07-14 15:27:27 +0000

    Initial commit

---
+++ Pattern-005/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void pattern2(int n) {
+               for (int i =0;i<=n-1;i++){
+                for (int j =0;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+               }
+    }
+}
